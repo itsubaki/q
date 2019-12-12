6c7e9d25e8bebb6bab1400b48f961e3b688bf481
itsubaki <itsubaki@users.noreply.github.com>
2019-12-12 16:01:58 +0900

    Update receiver name

--- pkg/math/vector/vector.go
+++ pkg/math/vector/vector.go
@@ -26,116 +26,118 @@ func NewZero(n int) Vector {
 	return v
 }
 
-func (v0 Vector) Clone() Vector {
+func (v Vector) Clone() Vector {
 	clone := Vector{}
-	for i := 0; i < len(v0); i++ {
-		clone = append(clone, v0[i])
+	for i := 0; i < len(v); i++ {
+		clone = append(clone, v[i])
 	}
 
 	return clone
 }
 
-func (v0 Vector) Dual() Vector {
+func (v Vector) Dual() Vector {
 	dual := Vector{}
-	for i := 0; i < len(v0); i++ {
-		dual = append(dual, cmplx.Conj(v0[i]))
+	for i := 0; i < len(v); i++ {
+		dual = append(dual, cmplx.Conj(v[i]))
 	}
 
 	return dual
 }
 
-func (v0 Vector) Add(v1 Vector) Vector {
-	v2 := Vector{}
-	for i := 0; i < len(v0); i++ {
-		v2 = append(v2, v0[i]+v1[i])
+func (v Vector) Add(v1 Vector) Vector {
+	add := Vector{}
+	for i := 0; i < len(v); i++ {
+		add = append(add, v[i]+v1[i])
 	}
 
-	return v2
+	return add
 }
 
-func (v0 Vector) Mul(z complex128) Vector {
-	v2 := Vector{}
-	for i := range v0 {
-		v2 = append(v2, z*v0[i])
+func (v Vector) Mul(z complex128) Vector {
+	mul := Vector{}
+	for i := range v {
+		mul = append(mul, z*v[i])
 	}
 
-	return v2
+	return mul
 }
 
-func (v0 Vector) TensorProduct(v1 Vector) Vector {
-	v2 := Vector{}
-	for i := 0; i < len(v0); i++ {
+func (v Vector) TensorProduct(v1 Vector) Vector {
+	p := Vector{}
+	for i := 0; i < len(v); i++ {
 		for j := 0; j < len(v1); j++ {
-			v2 = append(v2, v0[i]*v1[j])
+			p = append(p, v[i]*v1[j])
 		}
 	}
 
-	return v2
+	return p
 }
 
 // <v1|v0>
-func (v0 Vector) InnerProduct(v1 Vector) complex128 {
+func (v Vector) InnerProduct(v1 Vector) complex128 {
 	dual := v1.Dual()
 
 	p := complex(0, 0)
-	for i := 0; i < len(v0); i++ {
-		p = p + v0[i]*dual[i]
+	for i := 0; i < len(v); i++ {
+		p = p + v[i]*dual[i]
 	}
 
 	return p
 }
 
 // |v0><v1|
-func (v0 Vector) OuterProduct(v1 Vector) matrix.Matrix {
+func (v Vector) OuterProduct(v1 Vector) matrix.Matrix {
 	dual := v1.Dual()
 
-	m := matrix.Matrix{}
-	for i := 0; i < len(v0); i++ {
-		v := make([]complex128, 0)
+	p := matrix.Matrix{}
+	for i := 0; i < len(v); i++ {
+		vv := make([]complex128, 0)
 		for j := 0; j < len(dual); j++ {
-			v = append(v, v0[i]*dual[j])
+			vv = append(vv, v[i]*dual[j])
 		}
-		m = append(m, v)
+
+		p = append(p, vv)
 	}
 
-	return m
+	return p
 }
 
-func (v0 Vector) IsOrthogonal(v1 Vector) bool {
-	return v0.InnerProduct(v1) == complex(0, 0)
+func (v Vector) IsOrthogonal(v1 Vector) bool {
+	return v.InnerProduct(v1) == complex(0, 0)
 }
 
-func (v0 Vector) Norm() complex128 {
-	return cmplx.Sqrt(v0.InnerProduct(v0))
+func (v Vector) Norm() complex128 {
+	return cmplx.Sqrt(v.InnerProduct(v))
 }
 
-func (v0 Vector) IsUnit() bool {
-	return v0.Norm() == complex(1, 0)
+func (v Vector) IsUnit() bool {
+	return v.Norm() == complex(1, 0)
 }
 
-func (v0 Vector) Apply(mat matrix.Matrix) Vector {
-	v := Vector{}
+func (v Vector) Apply(mat matrix.Matrix) Vector {
+	apply := Vector{}
 
 	m, _ := mat.Dimension()
 	for i := 0; i < m; i++ {
 		tmp := complex(0, 0)
-		for j := 0; j < len(v0); j++ {
-			tmp = tmp + mat[i][j]*v0[j]
+		for j := 0; j < len(v); j++ {
+			tmp = tmp + mat[i][j]*v[j]
 		}
-		v = append(v, tmp)
+
+		apply = append(apply, tmp)
 	}
 
-	return v
+	return apply
 }
 
-func (v0 Vector) Equals(v1 Vector, eps ...float64) bool {
-	if len(v0) != len(v1) {
+func (v Vector) Equals(v1 Vector, eps ...float64) bool {
+	if len(v) != len(v1) {
 		return false
 	}
 
 	e := matrix.Eps(eps...)
-	for i := 0; i < len(v0); i++ {
-		if cmplx.Abs(v0[i]-v1[i]) > e {
+	for i := 0; i < len(v); i++ {
+		if cmplx.Abs(v[i]-v1[i]) > e {
 			return false
 		}
 	}
@@ -143,28 +145,28 @@ func (v0 Vector) Equals(v1 Vector, eps ...float64) bool {
 	return true
 }
 
-func (v0 Vector) Dimension() int {
-	return len(v0)
+func (v Vector) Dimension() int {
+	return len(v)
 }
 
-func TensorProductN(v0 Vector, bit ...int) Vector {
+func TensorProductN(v Vector, bit ...int) Vector {
 	if len(bit) < 1 {
-		return v0
+		return v
 	}
 
-	v1 := v0
+	p := v
 	for i := 1; i < bit[0]; i++ {
-		v1 = v1.TensorProduct(v0)
+		p = p.TensorProduct(v)
 	}
 
-	return v1
+	return p
 }
 
-func TensorProduct(v0 ...Vector) Vector {
-	v1 := v0[0]
-	for i := 1; i < len(v0); i++ {
-		v1 = v1.TensorProduct(v0[i])
+func TensorProduct(v ...Vector) Vector {
+	p := v[0]
+	for i := 1; i < len(v); i++ {
+		p = p.TensorProduct(v[i])
 	}
 
-	return v1
+	return p
 }
