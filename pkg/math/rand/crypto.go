810cdeb4848d529c738ef293a62b465fb03dc350
itsubaki <1759459+itsubaki@users.noreply.github.com>
2020-09-27 14:49:16 +0900

    Update CryptoInt

--- pkg/math/rand/crypto.go
+++ pkg/math/rand/crypto.go
@@ -8,25 +8,23 @@ import (
 )
 
 func Crypto(_ ...int64) float64 {
-	n := CryptoInt(1000)
-	v := n.Int64()
+	v := CryptoInt(1000)
 	return float64(v) / 1000
 }
 
-func CryptoInt(max int64) *big.Int {
-	n, err := rand.Int(rand.Reader, big.NewInt(max))
+func CryptoInt(max int) int {
+	n, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
 	if err != nil {
 		panic(err)
 	}
 
-	return n
+	return int(n.Int64())
 }
 
 func Coprime(N int) int {
 	min, max := 2, (N - 1)
 	for {
-		v := CryptoInt(int64(max - min))
-		a := int(v.Int64()) + min
+		a := CryptoInt(max-min) + min
 		if number.GCD(N, a) == 1 {
 			return a
 		}
