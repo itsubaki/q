d2f13f3835e404042b0ab3b9790772982b79a64f
itsubaki <1759459+itsubaki@users.noreply.github.com>
2020-09-27 23:36:14 +0900

    Update gcd_test.go

--- pkg/math/number/gcd_test.go
+++ pkg/math/number/gcd_test.go
@@ -3,7 +3,21 @@ package number
 import "testing"
 
 func TestGCD(t *testing.T) {
-	if GCD(15, 7) != 1 {
-		t.Fail()
+	cases := []struct {
+		a, b, c int
+	}{
+		{15, 2, 1},
+		{15, 4, 1},
+		{15, 7, 1},
+		{15, 11, 1},
+		{15, 13, 1},
+		{15, 14, 1},
+	}
+
+	for _, c := range cases {
+		gcd := GCD(c.a, c.b)
+		if gcd != c.c {
+			t.Errorf("gcd=%d", gcd)
+		}
 	}
 }
