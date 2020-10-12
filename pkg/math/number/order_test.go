1365dc3cf7e89e029da17b7bbe26db522380abcd
itsubaki <1759459+itsubaki@users.noreply.github.com>
2020-10-12 21:56:22 +0900

    Update ExampleFindOrder

--- pkg/math/number/order_test.go
+++ pkg/math/number/order_test.go
@@ -7,12 +7,37 @@ import (
 	"github.com/itsubaki/q/pkg/math/number"
 )
 
-func ExampleFindOrder() {
-	s, r, d, ok := number.FindOrder(2, 21, []int{0, 0, 1, 0, 1, 0, 1, 0, 1})
-	fmt.Printf("%v/%v=%v %v %v\n", s, r, d, ok, number.ModExp(2, r, 21))
+func ExampleFindOrder_mod15() {
+	m := []int{1, 1, 0}
+	s, r, d, ok := number.FindOrder(7, 15, m)
+	fmt.Printf("%v/%v=%v %v\n", s, r, d, ok)
+	fmt.Printf("%d^%d mod %d = %v\n", 7, r, 15, number.ModExp(7, r, 15))
 
 	// Output:
-	// 1/6=0.16666666666666666 true 1
+	// 3/4=0.75 true
+	// 7^4 mod 15 = 1
+}
+
+func ExampleFindOrder_mod21a2() {
+	m := []int{0, 0, 1, 0, 1, 0, 1, 0, 1}
+	s, r, d, ok := number.FindOrder(2, 21, m)
+	fmt.Printf("%v/%v=%v %v\n", s, r, d, ok)
+	fmt.Printf("%d^%d mod %d = %v\n", 2, r, 21, number.ModExp(2, r, 21))
+
+	// Output:
+	// 1/6=0.16666666666666666 true
+	// 2^6 mod 21 = 1
+}
+
+func ExampleFindOrder_mod21a4() {
+	m := []int{0, 1, 0, 1, 0, 1, 0, 1}
+	s, r, d, ok := number.FindOrder(4, 21, m)
+	fmt.Printf("%v/%v=%v %v\n", s, r, d, ok)
+	fmt.Printf("%d^%d mod %d = %v\n", 4, r, 21, number.ModExp(4, r, 21))
+
+	// Output:
+	// 1/3=0.3333333333333333 true
+	// 4^3 mod 21 = 1
 }
 
 func TestFindOrder(t *testing.T) {
