69be746f4796eb45308047ee7c667ca8c69ff732
itsubaki <1759459+itsubaki@users.noreply.github.com>
2020-10-14 21:37:06 +0900

    Refactor ParseFloat

--- pkg/math/number/parse.go
+++ pkg/math/number/parse.go
@@ -8,32 +8,36 @@ import (
 )
 
 func ParseFloat(binary string) (float64, error) {
+	for _, b := range binary {
+		if b == '0' || b == '1' || b == '.' {
+			continue
+		}
+
+		return 0, fmt.Errorf("invalid parameter. binary=%v", binary)
+	}
+
 	bin := strings.Split(binary, ".")
 	if len(bin) > 2 {
 		return 0, fmt.Errorf("invalid parameter. binary=%v", binary)
 	}
 
-	bi, err := strconv.ParseInt(bin[0], 2, 0)
+	p, err := strconv.ParseInt(bin[0], 2, 0)
 	if err != nil {
 		return 0, fmt.Errorf("parse int. binary=%v", binary)
 	}
 
-	d := float64(bi)
+	f := float64(p)
 	if len(bin) < 2 {
-		return d, nil
+		return f, nil
 	}
 
 	for i, b := range bin[1] {
-		if b != '0' && b != '1' {
-			return 0, fmt.Errorf("invalid parameter. binary=%v", binary)
-		}
-
 		if b == '0' {
 			continue
 		}
 
-		d = d + math.Pow(0.5, float64(i+1))
+		f = f + math.Pow(0.5, float64(i+1))
 	}
 
-	return d, nil
+	return f, nil
 }
