c37ff9112050a7180d4a58190cf0754841b879aa
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-03-07 12:17:12 +0900

    Update q_test.go

--- q_test.go
+++ q_test.go
@@ -175,19 +175,14 @@ func ExampleQ_Apply() {
 	// [11][  3]( 0.7071 0.0000i): 0.5000
 }
 
-func ExampleQ_qFT() {
+func ExampleQ_QFT() {
 	qsim := q.New()
 
 	q0 := qsim.Zero()
 	q1 := qsim.One()
 	q2 := qsim.Zero()
 
-	qsim.H(q0)
-	qsim.CR(q1, q0, 2)
-	qsim.CR(q2, q0, 3)
-	qsim.H(q1)
-	qsim.CR(q2, q1, 2)
-	qsim.H(q2)
+	qsim.QFT(q0, q1, q2)
 	qsim.Swap(q0, q2)
 
 	for _, s := range qsim.State() {
@@ -502,7 +497,12 @@ func Example_qFT() {
 	q1 := qsim.One()
 	q2 := qsim.Zero()
 
-	qsim.QFT(q0, q1, q2)
+	qsim.H(q0)
+	qsim.CR(q1, q0, 2)
+	qsim.CR(q2, q0, 3)
+	qsim.H(q1)
+	qsim.CR(q2, q1, 2)
+	qsim.H(q2)
 	qsim.Swap(q0, q2)
 
 	for _, s := range qsim.State() {
