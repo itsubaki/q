95d537a86624930ee2276b56d12cf65bd0aac0d8
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-04-03 16:08:12 +0900

    Update ParseFloat

--- pkg/math/number/parse.go
+++ pkg/math/number/parse.go
@@ -16,18 +16,18 @@ func ParseFloat(binary string) (float64, error) {
 		return 0, fmt.Errorf("invalid parameter. binary=%v", binary)
 	}
 
+	if !strings.Contains(binary, ".") {
+		p := Must(strconv.ParseInt(binary, 2, 0))
+		return float64(p), nil
+	}
+
 	bin := strings.Split(binary, ".")
-	if len(bin) > 2 {
+	if len(bin) != 2 {
 		return 0, fmt.Errorf("invalid parameter. binary=%v", binary)
 	}
 
 	p := Must(strconv.ParseInt(bin[0], 2, 0))
 	v := float64(p)
-
-	if len(bin) < 2 {
-		return v, nil
-	}
-
 	for i, b := range bin[1] {
 		if b == '0' {
 			continue
