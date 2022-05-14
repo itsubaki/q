84bcbe88882323f6049ed1a03da356641c171150
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-05-14 22:23:01 +0900

    Rename MustCryptoInt64

--- pkg/math/rand/crypto.go
+++ pkg/math/rand/crypto.go
@@ -9,20 +9,20 @@ import (
 )
 
 func Crypto(_ ...int) float64 {
-	v := CryptoInt64(0, math.MaxInt64)
+	v := MustCryptoInt64(0, math.MaxInt64)
 	return float64(v) / math.MaxInt64
 }
 
 func Coprime(N int) int {
 	for {
-		a := int(CryptoInt64(2, int64(N-1)))
+		a := int(MustCryptoInt64(2, int64(N-1)))
 		if number.GCD(N, a) == 1 {
 			return a
 		}
 	}
 }
 
-func CryptoInt64(min, max int64) int64 {
+func MustCryptoInt64(min, max int64) int64 {
 	v := number.Must(rand.Int(rand.Reader, big.NewInt(max-min)))
 	return v.Int64() + min
 }
