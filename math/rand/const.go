5a016b5745cc61fb4eeb55d425f9ab1b731ecf14
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-03-21 23:18:11 +0900

    Fix comment

--- math/rand/const.go
+++ math/rand/const.go
@@ -2,7 +2,7 @@ package rand
 
 import "math/rand"
 
-// Const returns a random float64 in [0, 1) with seed.
+// Const returns a constant number in [0.0, 1.0).
 func Const(seed ...int) func() float64 {
 	s := 1
 	if len(seed) > 0 {
