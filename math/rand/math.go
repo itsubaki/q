54f1fb230e4e8824d5f293269d9acd4608e3f5aa
itsubaki <1759459+itsubaki@users.noreply.github.com>
2024-04-06 04:41:41 +0000

    Replace crypto/rand to math/rand/v2

--- math/rand/math.go
+++ math/rand/math.go
@@ -1,5 +1,34 @@
 package rand
 
-import "math/rand/v2"
+import (
+	"math/rand/v2"
+
+	"github.com/itsubaki/q/math/number"
+)
 
 var Float64 = rand.Float64
+
+// Const returns a constant number in [0.0, 1.0).
+func Const(seed ...uint64) func() float64 {
+	var s0, s1 uint64
+	if len(seed) > 0 {
+		s0 = seed[0]
+	}
+
+	if len(seed) > 1 {
+		s1 = seed[1]
+	}
+
+	return rand.New(rand.NewPCG(s0, s1)).Float64
+}
+
+// Coprime returns a random coprime number in [2, N).
+func Coprime(N int) int {
+	min, max := 2, N-2
+	for {
+		a := rand.N(max-1) + min
+		if number.GCD(N, a) == 1 {
+			return a
+		}
+	}
+}
--- math/rand/math_test.go
+++ math/rand/math_test.go
@@ -1,13 +1,65 @@
 package rand_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/itsubaki/q/math/rand"
 )
 
+func ExampleCoprime() {
+	p := rand.Coprime(15)
+
+	for _, e := range []int{2, 4, 7, 8, 11, 13, 14} {
+		if p == e {
+			fmt.Println("found")
+			break
+		}
+	}
+
+	// Output:
+	// found
+}
+
+func ExampleConst() {
+	c := rand.Const()
+	fmt.Printf("%.13f\n", c())
+	fmt.Printf("%.13f\n", c())
+	fmt.Printf("%.13f\n", c())
+	fmt.Printf("%.13f\n", rand.Const(1)())
+	fmt.Printf("%.13f\n", rand.Const(1)())
+	fmt.Printf("%.13f\n", rand.Const(1)())
+	fmt.Printf("%.13f\n", rand.Const(2)())
+	fmt.Printf("%.13f\n", rand.Const(3)())
+	fmt.Printf("%.13f\n", rand.Const(1, 0)())
+	fmt.Printf("%.13f\n", rand.Const(1, 1)())
+	fmt.Printf("%.13f\n", rand.Const(1, 2)())
+
+	// Output:
+	// 0.9999275824803
+	// 0.8856419373529
+	// 0.3814775277115
+	// 0.2384231908739
+	// 0.2384231908739
+	// 0.2384231908739
+	// 0.8269781200925
+	// 0.8353847703964
+	// 0.2384231908739
+	// 0.3402859786606
+	// 0.6764556596678
+}
+
 func TestFloat64(t *testing.T) {
 	r := rand.Float64()
+	if r >= 0 && r < 1 {
+		return
+	}
+
+	t.Fail()
+}
+
+func TestConst(t *testing.T) {
+	r := rand.Const()()
 	if r >= 0 && r < 1 {
 		return
 	}
