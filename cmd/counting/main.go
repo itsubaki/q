73b9f4399e399caf00c4a3cb3a447d6929017b1b
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-01-25 00:39:46 +0900

    Revert counting

--- cmd/counting/main.go
+++ cmd/counting/main.go
@@ -12,7 +12,7 @@ import (
 // The number of solutions `M` is 2.
 func controlledG(qsim *q.Q, r, s []q.Qubit, c, a q.Qubit) {
 	oracle(qsim, r, s, c, a)
-	diffuser(qsim, c, r)
+	diffuser(qsim, r)
 }
 
 func oracle(qsim *q.Q, r, s []q.Qubit, c, a q.Qubit) {
@@ -27,7 +27,7 @@ func oracle(qsim *q.Q, r, s []q.Qubit, c, a q.Qubit) {
 	xor(r[1], r[3], s[3]) // b != d
 
 	// apply Z if s and c are all 1
-	qsim.ControlledZ(append(s, c), []q.Qubit{a})
+	qsim.ControlledZ([]q.Qubit{s[0], s[1], s[2], s[3], c}, []q.Qubit{a})
 
 	// uncompute
 	xor(r[1], r[3], s[3])
@@ -36,10 +36,10 @@ func oracle(qsim *q.Q, r, s []q.Qubit, c, a q.Qubit) {
 	xor(r[0], r[1], s[0])
 }
 
-func diffuser(qsim *q.Q, c q.Qubit, r []q.Qubit) {
+func diffuser(qsim *q.Q, r []q.Qubit) {
 	qsim.H(r...)
 	qsim.X(r...)
-	qsim.ControlledZ(append([]q.Qubit{c}, r[:len(r)-1]...), []q.Qubit{r[len(r)-1]})
+	qsim.ControlledZ(r[:len(r)-1], []q.Qubit{r[len(r)-1]})
 	qsim.X(r...)
 	qsim.H(r...)
 }
