4de42405501d00795051eb64637da0b5f2b8cd2a
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-03-15 19:44:23 +0900

    Update Pow

--- math/number/pow.go
+++ math/number/pow.go
@@ -8,7 +8,7 @@ func Ldexp(a, b int) float64 {
 }
 
 // Pow returns a**r, the base-a exponential of r.
-func Pow(a, r int) int {
+func Pow[T ~int | ~int64](a, r T) T {
 	if a == 0 {
 		return 0
 	}
@@ -18,7 +18,7 @@ func Pow(a, r int) int {
 	}
 
 	// exponentiation by squaring
-	p := 1
+	p := T(1)
 	for r > 0 {
 		if r&1 == 1 {
 			p = p * a
