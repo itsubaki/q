eed623c911b36d490a62f66733d4f4c9907d7c74
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-08-28 17:12:55 +0900

    Add godoc

--- function/fredkin.go
+++ function/fredkin.go
@@ -3,6 +3,7 @@ package function
 import "github.com/itsubaki/q"
 
 // Fredkin applies the Fredkin gate.
+// It swaps the target qubits target0 and target1 if the control qubit is 1.
 func Fredkin(qsim *q.Q, control q.Qubit, target0, target1 q.Qubit) {
 	qsim.CNOT(target0, target1)
 	qsim.CCNOT(control, target1, target0)
--- function/xor.go
+++ function/xor.go
@@ -2,6 +2,8 @@ package function
 
 import "github.com/itsubaki/q"
 
+// XOR applies the XOR operation.
+// It flips the target qubit z if either x or y is 1, but not both.
 func XOR(qsim *q.Q, x, y, z q.Qubit) {
 	qsim.CNOT(x, z)
 	qsim.CNOT(y, z)
